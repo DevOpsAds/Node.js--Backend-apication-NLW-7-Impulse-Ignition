02a5ee30ed2a59d9fff48f24da7b5f0e0ad39b6c
DevOps-full-jr <joaobatistalimajunior.ads@gmail.com>
2022-04-18 17:49:02 -0300

    Error em user_id=sub

---
+++ src/@types/express/index.ts
@@ -0,0 +1,19 @@
+
+
+
+// export interface Express {
+//     user_id: string;
+  
+//  }
+ 
+//  var persons: { [id: string]: IPerson; } = {
+//     "p1": { firstName: "F1", lastName: "L1" },
+//     "p2": { firstName: "F2" }
+//  };
+
+declare namespace Express{
+    export interface Request{
+        user_id: string;
+    }
+}
+
