0a6ef76d598060bbae02fc4daad59c6c24e6ff91
DevOps-full-jr <joaobatistalimajunior.ads@gmail.com>
2022-04-19 12:45:59 -0300

    ok finalizado envio de mensagem partimos para instalaçao do socket_io

--- src/routes.ts
+++ src/routes.ts
@@ -5,7 +5,7 @@ import { ensureAuthenticated } from './middleware/ensureAuthenticated'
 
 const router=Router()
 router.post("/authenticate",new AuthenticateUserController().handle)
-router.post("messages",ensureAuthenticated,new CreateMessageController().handle)
+router.post("/message",ensureAuthenticated,new CreateMessageController().handle)
 
 
 export {router}
